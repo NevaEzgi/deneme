053623f410655c864cfbb52b26057e1b9f87cc53
NevaEzgi <150270359+NevaEzgi@users.noreply.github.com>
2023-11-08 20:19:28 +0300

    Add files via upload

---
+++ pro/deneme.py
@@ -0,0 +1,27 @@
+import random
+
+sifreleme_a = "abcdefghABCDEFGH"
+sifreleme_n = "12345678"
+sifreleme_s = "/&%+*?-_"
+
+parola_l = int(input("parolanızın uzunluğunu belirleyiniz:"))
+
+parola = ""
+
+sayim = 0
+
+for i in range(parola_l):
+    
+        parola += random.choice(sifreleme_a)
+        sayim += 1
+
+        parola += random.choice(sifreleme_n)
+        sayim += 1
+
+        parola += random.choice(sifreleme_s)
+        sayim += 1
+
+        if sayim == parola_l:
+                break
+
+print(parola)
